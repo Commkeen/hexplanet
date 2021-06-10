0610ce141c01a082d7cbbaf9969e637678e9e614
Matt Everett <mceverett@cavernlore.com>
2021-06-09 18:42:07 -0700

    Adjustable water coverage

--- Assets/Scripts/TerrainFilters/ElevationFilter.cs
+++ Assets/Scripts/TerrainFilters/ElevationFilter.cs
@@ -19,10 +19,15 @@ public class ElevationFilter
             noise = new Noise(settings.seed);
         }
 
+        if (settings.minElevation > settings.maxElevation) {settings.maxElevation = settings.minElevation;}
+
         var noisePosition = cell.center * settings.roughness;
         noisePosition += settings.center;
 
-        float noiseResult = noise.Evaluate(noisePosition)*.5f +.5f;
+        float noiseResult = noise.Evaluate(noisePosition)*.5f + .5f;
+
+        if (noiseResult < settings.waterCoverage) {noiseResult = 0;}
+        noiseResult = Mathf.InverseLerp(settings.waterCoverage, 1f, noiseResult);
 
         int result = Mathf.FloorToInt(Mathf.Lerp(settings.minElevation, settings.maxElevation, noiseResult));
         result = Mathf.Clamp(result, settings.minElevation, settings.maxElevation);
--- Assets/Scripts/TerrainFilters/ElevationSettings.cs
+++ Assets/Scripts/TerrainFilters/ElevationSettings.cs
@@ -6,7 +6,12 @@ public class ElevationSettings : ScriptableObject
 {
     public int seed = 0;
     public Vector3 center = Vector3.zero;
+    [Min(0)]
     public float roughness = 1;
+    [Min(0)]
     public int minElevation = 0;
+    [Min(0)]
     public int maxElevation = 8;
+    [Range(0f,1f)]
+    public float waterCoverage = 1;
 }
